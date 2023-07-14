921d8de21c0327201ca9eac783854898509dab8e
Slava Yakimov <fintechmustreets@gmail.com>
2023-07-14 10:04:17 +0500

    assets

--- src/assets/svg/next.ts
+++ src/assets/svg/next.ts
@@ -1,5 +1,12 @@
 export default `
-<svg width="8" height="16" viewBox="0 0 8 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M1 1.50006L7 8.00006L1 14.5001" stroke="#AEAEAE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<svg width="10" height="16" viewBox="0 0 10 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1 1L8 8L1 15" stroke="black" stroke-width="1.5"/>
+</svg>
+`;
+
+export const checkboxSvg = `
+<svg width="36" height="20" viewBox="0 0 36 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="10" cy="10" r="7" fill="black"/>
+<rect x="0.5" y="0.5" width="35" height="19" rx="9.5" stroke="black"/>
 </svg>
 `;
